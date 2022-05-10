7d49f9015c6250ce4e610a56aa270e0b5cf7c9f0
André Santos <andrefs@andrefs.com>
2022-05-10 22:17:56 +0100

    Copied old settings

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -4,10 +4,10 @@ function M.config()
   local status_ok, toggleterm = pcall(require, "toggleterm")
   if status_ok then
     toggleterm.setup(astronvim.user_plugin_opts("plugins.toggleterm", {
-      size = 10,
+      size = 60,
       open_mapping = [[<c-\>]],
       shading_factor = 2,
-      direction = "float",
+      direction = "vertical",
       float_opts = {
         border = "curved",
         highlights = {
